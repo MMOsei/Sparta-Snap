57fe301cf6521497a992cd8a0e0e0be37fe9b7e1
mmosei <mmanly-osei@spartaglobal.com>
2018-06-14 18:58:54 +0100

    Create working code that stores 2 seperate clicks

--- JS/app.js
+++ JS/app.js
@@ -1,37 +1,55 @@
 $(document).ready(function(){
 
-  var colours = [
-  'red',
-  'yellow',
-  'orange',
-  'blue',
-  'green',
-  'purple',
-  'white',
-  'black',
-  'purple',
-  'white',
-  'blue',
-  'green',
-  'orange',
-  'black',
-  'yellow',
-  'red'
-  ];
-  console.log(colours);
-ge
+  // var colours = [
+  // 'red',
+  // 'yellow',
+  // 'orange',
+  // 'blue',
+  // 'green',
+  // 'purple',
+  // 'white',
+  // 'black',
+  // 'purple',
+  // 'white',
+  // 'blue',
+  // 'green',
+  // 'orange',
+  // 'black',
+  // 'yellow',
+  // 'red'
+  // ];
+  // console.log(colours);
+
   var box = document.getElementsByClassName('box');
-  console.log(box);
+  // console.log(box);
   var boxes = [...box]
   console.log(boxes);
+
+  var click1 = "";
+  var click2 = "";
+
   // // var lettersArray;
-  var index = Math.floor(Math.random() * colours.length);
-  console.log(index);
-  console.log(colours.length);
+  // var index = Math.floor(Math.random() * colours.length);
+  // console.log(index);
+  // console.log(colours.length);
   // //var id;
   // //var livesLeft = 6;
+
+
+  $(this).click(function(event){
+
+    if (click1 == "") {
+      click1 = event.target.innerHTML;
+      console.log(click1);
+    } else {
+      click2 = event.target.innerHTML;
+      console.log(click2);
+    }
+  });
   //
-  //
+
+
+
   //This generates a random word to be guessed
   $('#play').one('click', function () {
     $('#play').toggle();
@@ -45,6 +63,13 @@ ge
   //   // $('#current-score').html(livesLeft);
   //   // move();
   });
+
+  // var = colourAtPlay
+
+  // pseudo code: box.click(function(){
+  // colourAtPlay = this.val() or html()
+  //
+  //
   //
   //
 
