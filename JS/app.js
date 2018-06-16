ae312b8fc1f0a0c498a8b5d98b765923a44ab604
mmosei <mmanly-osei@spartaglobal.com>
2018-06-16 11:55:24 +0100

    got the randomiser function to shuffle boxes

--- JS/app.js
+++ JS/app.js
@@ -1,76 +1,102 @@
 $(document).ready(function(){
-
-  // var colours = [
-  // 'red',
-  // 'yellow',
-  // 'orange',
-  // 'blue',
-  // 'green',
-  // 'purple',
-  // 'white',
-  // 'black',
-  // 'purple',
-  // 'white',
-  // 'blue',
-  // 'green',
-  // 'orange',
-  // 'black',
-  // 'yellow',
-  // 'red'
-  // ];
-  // console.log(colours);
+  $('#the-grid').toggle();
+  // hide();
 
   var box = document.getElementsByClassName('box');
   // console.log(box);
-  var boxes = [...box]
-  console.log(boxes);
+  var boxes = [...box];
+  // console.log(boxes);
 
   var click1 = "";
   var click2 = "";
+  var index = Math.floor(Math.random() * boxes.length);
+
+  // console.log(index);
 
   // // var lettersArray;
-  // var index = Math.floor(Math.random() * colours.length);
   // console.log(index);
   // console.log(colours.length);
   // //var id;
-  // //var livesLeft = 6;
 
+
+  $('#play').one('click', function () {
+    $('#the-grid').toggle();
+    $('#play').toggle();
+    shuffle();
+  });
+
+
+// if ($('#play') == (":hidden")) {
 
   $(this).click(function(event){
-
-    if (click1 == "") {
-      click1 = event.target.innerHTML;
-      console.log(click1);
-    } else {
-      click2 = event.target.innerHTML;
-      console.log(click2);
-    }
+    // $('.white').toggle();
+    // var currentBox = event.target.innerHTML
+    // console.log(currentBox);
+    // console.log(event);
+    gamePlayClick();
   });
+// }
   //
 
 
+  function shuffle() {
+    var myArray = boxes;
+    var currentPass = myArray.length;
+    var index, temp;
 
-  //This generates a random word to be guessed
-  $('#play').one('click', function () {
-    $('#play').toggle();
-  //   //submitGuess();
-  //   //$('.game-area').toggle(); //turns on the game area once the play button is clicked
-  //
-    for (var i = 0; i < colours[index]; i++) {
-      // var box = $('<div class="box">' + colours + '</div>');
-      $('.box').html(colours);
+    while (currentPass >0) {
+      index = Math.floor(Math.random()*currentPass);
+      currentPass--;
+
+      temp = myArray[currentPass];
+      myArray[currentPass] = myArray[index];
+      myArray[index] = temp;
     }
-  //   // $('#current-score').html(livesLeft);
-  //   // move();
-  });
 
-  // var = colourAtPlay
+    console.log(myArray);
+  // document.getElementByClassName('box') = myArray;
+  for (var i = 0; i < myArray.length; i++) {
+      boxes = $('<div class="box">' + myArray + '</div>');
+      $('#the-grid').append(myArray);
+    }
 
-  // pseudo code: box.click(function(){
-  // colourAtPlay = this.val() or html()
-  //
-  //
-  //
-  //
+  }
+
+
+
+  // Function that controlls the game play clicks
+
+  var count = 0;
+  function gamePlayClick() {
+    // for (var i = 0; i < 2; i++) {
+
+      if (click1 == "") {
+        click1 = event.target.innerHTML;
+        count++;
+        console.log(click1);
+        console.log(count);
+      } else {
+        click2 = event.target.innerHTML;
+        count++;
+        console.log(click2);
+        console.log(count);
+        // if the 2 clicks match then leave it uncovered
+        if (click1 === click2) {
+          console.log('I\'ts a match!');
+          // if they do not match then they should hide again
+
+          click1 = '';
+          click2 = '';
+        }  else {
+          count = 0;
+          console.log('That\'s not a match' + count);
+          click1 = '';
+          click2 = '';
+        } // Second level if statement close
+      } //First level if statement close
+    // } // For loop close
+  } // Function close
+
+
 
 });
