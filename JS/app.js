0fe5cf7d583ff17d056104864825184edd268886
mmosei <mmanly-osei@spartaglobal.com>
2018-06-17 21:37:42 +0100

    update

--- JS/app.js
+++ JS/app.js
@@ -1,44 +1,27 @@
 $(document).ready(function(){
-  $('#the-grid').toggle();
-  // hide();
 
   var box = document.getElementsByClassName('box');
-  // console.log(box);
   var boxes = [...box];
-  // console.log(boxes);
-
   var click1 = "";
   var click2 = "";
-  var index = Math.floor(Math.random() * boxes.length);
+  var win = 0;
 
-  // console.log(index);
+  // $('#play').one('click', function () {
+  //   $('#the-grid').toggle();
+  //   $('#play').toggle();
+  // });
 
-  // // var lettersArray;
-  // console.log(index);
-  // console.log(colours.length);
-  // //var id;
+  //-------------------------------------
 
-
-  $('#play').one('click', function () {
-    $('#the-grid').toggle();
-    $('#play').toggle();
-    shuffle();
-  });
-
-
-// if ($('#play') == (":hidden")) {
+  shuffle();
 
   $(this).click(function(event){
-    // $('.white').toggle();
-    // var currentBox = event.target.innerHTML
-    // console.log(currentBox);
-    // console.log(event);
     gamePlayClick();
   });
-// }
-  //
 
+//-------------------------------------
 
+  //This is the shuffle function
   function shuffle() {
     var myArray = boxes;
     var currentPass = myArray.length;
@@ -51,52 +34,50 @@ $(document).ready(function(){
       temp = myArray[currentPass];
       myArray[currentPass] = myArray[index];
       myArray[index] = temp;
-    }
+    } // while loop closing bracket
+    for (var i = 0; i < myArray.length; i++) {
+        boxes = $('<div class="box">' + myArray + '</div>');
+        $('#the-grid').append(myArray);
+        // $('.box').append.addClass("white");
+      } // for loop closing bracket
+  } // shuffle fintion close
 
-    console.log(myArray);
-  // document.getElementByClassName('box') = myArray;
-  for (var i = 0; i < myArray.length; i++) {
-      boxes = $('<div class="box">' + myArray + '</div>');
-      $('#the-grid').append(myArray);
-    }
-
-  }
-
-
-
-  // Function that controlls the game play clicks
-
+  // Function that controls the game play clicks
   var count = 0;
   function gamePlayClick() {
-    // for (var i = 0; i < 2; i++) {
 
-      if (click1 == "") {
-        click1 = event.target.innerHTML;
-        count++;
-        console.log(click1);
-        console.log(count);
-      } else {
-        click2 = event.target.innerHTML;
-        count++;
-        console.log(click2);
-        console.log(count);
-        // if the 2 clicks match then leave it uncovered
-        if (click1 === click2) {
-          console.log('I\'ts a match!');
-          // if they do not match then they should hide again
+    if (click1 == "") {
+      click1 = event.target.innerHTML;
+      count++;
+      console.log(click1);
+      console.log(count);
+    } else {
+      click2 = event.target.innerHTML;
+      count++;
+      console.log(click2);
+      console.log(count);
 
-          click1 = '';
-          click2 = '';
+      // winning condition
+      if (click1 === click2) {
+        win++;
+        console.log('I\'ts a match!');
+        console.log('matched ' + win);
+        if (win == 8) {
+        console.log('Winner');
+        click1 = '';
+        click2 = '';
         }  else {
-          count = 0;
-          console.log('That\'s not a match' + count);
-          click1 = '';
-          click2 = '';
+        console.log('That\'s not a match' + count);
+        click1 = '';
+        click2 = '';
         } // Second level if statement close
       } //First level if statement close
-    // } // For loop close
+    } // else statement close
+    //losing condition
+    if (count == 20) {
+      console.log('You lose!!');
+    } // Lose condition if statement close
   } // Function close
-
 
 
 });
